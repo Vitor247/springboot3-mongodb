86ccd1bce28ddbc5c3d281b4dc88fda4f5378c03
Vitor Camilo <vittor.camilo@gmail.com>
2025-10-20 18:24:59 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/vitorcamilodev/mongo/resources/UserResource.java
+++ src/main/java/com/vitorcamilodev/mongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.vitorcamilodev.mongo.domain.Post;
 import com.vitorcamilodev.mongo.domain.User;
 import com.vitorcamilodev.mongo.dto.UserDTO;
 import com.vitorcamilodev.mongo.services.UserService;
@@ -62,4 +63,10 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
+	}
+	
 }
