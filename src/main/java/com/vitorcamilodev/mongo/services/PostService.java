05a4c721674b3be0accd3e84342d1201075f099b
Vitor Camilo <vittor.camilo@gmail.com>
2025-10-20 18:53:43 -0300

    Obtendo um post por id

---
+++ src/main/java/com/vitorcamilodev/mongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.vitorcamilodev.mongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vitorcamilodev.mongo.domain.Post;
+import com.vitorcamilodev.mongo.exception.ObjectNotFoundException;
+import com.vitorcamilodev.mongo.repositories.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> optional = postRepository.findById(id);
+		return optional.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
